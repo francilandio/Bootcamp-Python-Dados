a56cb0e7c228c92861ffd65c3d5bd3d935db657e
Francilândio <francilandioo@gmail.com>
2024-06-23 16:17:54 -0300

    Conclusão exercício 01

---
+++ Introdução/Exercicio_01.py
@@ -0,0 +1,4 @@
+# Crie um programa que o usuário digite o nome e retorna o número de caracteres
+
+qtd_caractere = len(input("Digite seu nome: "))
+print(f"Seu nome tem {qtd_caractere} caracteres.")

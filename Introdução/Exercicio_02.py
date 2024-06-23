89c76a44cc1ea11b9b92bed07d15b43ea0cbdc2d
Francilândio <francilandioo@gmail.com>
2024-06-23 16:25:48 -0300

    Inclusão de exercício 02

---
+++ Introdução/Exercicio_02.py
@@ -0,0 +1,6 @@
+# Crie um programa onde o usuário digite dois valores e seja exibido a soma no final
+
+valor1 = float(input("Digite um valor: "))
+valor2 = float(input("Digite outro valor: "))
+
+print(f"A soma entre os dois valores é: {valor1 + valor2}")

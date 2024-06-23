2cc661a09545385d06f4620b0b34f14f9edb06ca
Francilândio <francilandioo@gmail.com>
2024-06-23 16:40:33 -0300

    Inclusão de constante

--- Introdução/Desafio.py
+++ Introdução/Desafio.py
@@ -1,12 +1,13 @@
 # Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal e o valor do bônus que recebeu. 
 # O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
 
+CONSTANTE_BONUS = 1000
 nome = input("Digite o seu nome: ")
 salario = float(input("Qual o seu salário: "))
 percentual_bonus = float(input("Qual o percentual de bônus? "))
 
 # Cálculo do KPI do bônus de 2024 é de 1.000 + salário * bônus
 
-resultado = 1000 + salario * percentual_bonus
+resultado = CONSTANTE_BONUS + salario * percentual_bonus
 
 print(f"Olá {nome}, o seu bônus é de {resultado}")

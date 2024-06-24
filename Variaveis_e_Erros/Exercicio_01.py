962c10eece1f8d1fca7d572f2b75ad96f049108a
Francilândio <francilandioo@gmail.com>
2024-06-24 15:15:13 -0300

    Exercício 01 concluído

---
+++ Variaveis_e_Erros/Exercicio_01.py
@@ -0,0 +1,8 @@
+# Escreva um programa que soma dois números inteiros pelo usuário.
+
+valor1 = float(input("Digite um valor: "))
+valor2 = float(input("Digite outro valor: "))
+
+resultado = valor1 + valor2
+
+print(f"A soma entre os dois valores é: {resultado}")

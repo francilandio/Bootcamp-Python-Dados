3d267a340476b890378d0c4b977d0ae92696dc51
Francilândio <103860916+francilandio@users.noreply.github.com>
2024-06-25 12:14:57 -0300

    Delete Variaveis_e_Erros/Exercicio_03.py

--- Variaveis_e_Erros/Exercicio_03.py
+++
@@ -1,8 +0,0 @@
-# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-
-numero1 = int(input("Insira um número inteiro: "))
-numero2 = int(input("Insira outro número inteiro: "))
-
-resultado_multiplicacao = numero1 * numero2
-
-print(f"O resultado da multiplicação entre os dois números é: {resultado_multiplicacao}")

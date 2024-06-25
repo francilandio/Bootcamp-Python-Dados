0fcd89c9bd394480739472db88e26e2cf1abec5c
Francilândio <103860916+francilandio@users.noreply.github.com>
2024-06-25 12:14:46 -0300

    Delete Variaveis_e_Erros/Exercicio_02.py

--- Variaveis_e_Erros/Exercicio_02.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-
-valor = int(input("Digite um número inteiro: "))
-
-resultado_resto = valor % 5
-
-print(f"O resto da divisão por 5 é: {resultado_resto}")

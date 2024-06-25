d942e43ec29a4325f498dcda629d4d03f88ad562
Francilândio <francilandioo@gmail.com>
2024-06-25 12:12:34 -0300

    Conclusão do Exercício 06

---
+++ Variaveis_e_Erros/Flutuante/Exercicio_06.py
@@ -0,0 +1,8 @@
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+valor1 = float(input("Digite um valor flutuante: "))
+valor2 = float(input("Digite outro valor flutuante: "))
+
+resultado = valor1 + valor2
+
+print(f"A soma entre os dois valores é: {resultado}")

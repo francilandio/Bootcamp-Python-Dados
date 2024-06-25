074b049f36a28048395b76e0519861dc3a369cae
Francilândio <103860916+francilandio@users.noreply.github.com>
2024-06-25 12:15:13 -0300

    Delete Variaveis_e_Erros/Exercicio_05.py

--- Variaveis_e_Erros/Exercicio_05.py
+++
@@ -1,7 +0,0 @@
-# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-
-numero1 = int(input("Insira um número inteiro: "))
-
-resultado_quadrado = numero1 ** 2
-
-print(f"O número fornecido ao quadrado é: {resultado_quadrado}")

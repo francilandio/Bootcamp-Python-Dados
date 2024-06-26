73e8dfc06630cd8dd738fa1b1384ecab2ba3fdc3
Francilândio <francilandioo@gmail.com>
2024-06-25 22:55:33 -0300

    Conclusão do Exercício 15

--- Variaveis_e_Erros/Strings/Exercicio_15.py
+++ Variaveis_e_Erros/Strings/Exercicio_15.py
@@ -1,1 +1,8 @@
 # Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+texto1 = input("Digite um texto: ")
+texto2 = input("Digite outro texto: ")
+
+texto_concatenado = texto1 + " " + texto2
+
+print(f"Texto concatenado: {texto_concatenado}")

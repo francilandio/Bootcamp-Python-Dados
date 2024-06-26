c7c0d2dc6fe81b2b5d0211c17425d54bdae1be1c
Francilândio <francilandioo@gmail.com>
2024-06-25 22:08:22 -0300

    Conclusão do Exercício 08

---
+++ Variaveis_e_Erros/Flutuante/Exercicio_08.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+
+potencia = base ** expoente
+
+print(f"O resultado da potência é: {potencia}")

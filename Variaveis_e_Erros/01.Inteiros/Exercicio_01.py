e9b3fc6bea45de31066fb33b921288478866a51e
Francilândio <francilandioo@gmail.com>
2024-06-25 22:36:51 -0300

    Ajuste de nome da pasta

---
+++ Variaveis_e_Erros/01.Inteiros/Exercicio_01.py
@@ -0,0 +1,8 @@
+# Escreva um programa que soma dois números inteiros pelo usuário.
+
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+
+resultado = valor1 + valor2
+
+print(f"A soma entre os dois valores é: {resultado}")

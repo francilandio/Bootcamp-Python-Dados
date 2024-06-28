8849ab7caf4aa0b8703229a68aac9d2983ac89a7
Francilândio <francilandioo@gmail.com>
2024-06-28 16:32:52 -0300

    Adição de tratamento de erro em caso de digitação errada

--- Variaveis_e_Exceções/Flutuante/Exercicio_09.py
+++ Variaveis_e_Exceções/Flutuante/Exercicio_09.py
@@ -1,7 +1,8 @@
 # Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
-celsius = float(input("Digite a temperatura em Celsius: "))
-
-fahrenheit = (celsius * 9/5) + 32
-
-print(f"{celsius}°C é igual a {fahrenheit}ºF")
+try:
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}°C é igual a {fahrenheit}ºF")
+except ValueError:
+    print("Por favor, digite um número válido para a temperatura.")

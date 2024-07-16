eb5de274fa83921dc38b426c378c8c7204da5d96
Francilândio <francilandioo@gmail.com>
2024-07-15 22:32:00 -0300

    Remorção de exceção com Try-Except

--- Variaveis_e_Exceções/Flutuante/Exercicio_09.py
+++ Variaveis_e_Exceções/Flutuante/Exercicio_09.py
@@ -1,8 +1,5 @@
 # Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
-try:
-    celsius = float(input("Digite a temperatura em Celsius: "))
-    fahrenheit = (celsius * 9/5) + 32
-    print(f"{celsius}°C é igual a {fahrenheit}ºF")
-except ValueError:
-    print("Por favor, digite um número válido para a temperatura.")
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C é igual a {fahrenheit}ºF")

c62830910c170dcf819f67c90164ed1554bce22b
Francilândio <francilandioo@gmail.com>
2024-07-21 11:21:13 -0300

    Criação e conclusão do Exercício 8

---
+++ Controle_de_Fluxo/For/Exercicio_8.py
@@ -0,0 +1,11 @@
+# Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando.
+
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+usuarios_validos = [usuario for usuario in usuarios if usuario["email"]]
+
+print(usuarios_validos)

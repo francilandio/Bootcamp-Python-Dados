8855d8bec3f8967c38cc6dd2bf285e9c940a1ec8
Francilândio <francilandioo@gmail.com>
2024-07-24 22:41:36 -0300

    Criação e conclusão do Exercicio 1

---
+++ Listas_e_Dicionários/Exercicio_1.py
@@ -0,0 +1,5 @@
+# Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+numeros = list(range(1, 11))
+for numero in numeros:
+    print(f"{numero} ao quadrado é {numero**2}")

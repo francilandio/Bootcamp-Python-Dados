53f73bb1a51fbeddc4cb68d1f30560f212f874c8
Francilândio <francilandioo@gmail.com>
2024-07-28 13:25:14 -0300

    Criação e conclusão do Exercício 6

---
+++ Listas_e_Dicionários/Exercicio_6.py
@@ -0,0 +1,6 @@
+# Dada uma lista de emails, remover todos os duplicados.
+
+emails = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
+emails_unicos = list(set(emails))
+
+print(emails_unicos)
